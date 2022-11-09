f7f9cdbc41132a9b8b6fb6700a9a8c9a938e8dbe
Tarcisio <caninosbrancos10031@gmail.com>
2022-11-08 21:28:40 -0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Andressa
+# Andressa
---
+++ src/Components/Home/index.jsx
@@ -0,0 +1,16 @@
+import Inicio from "../Pages/Inicio";
+// import Menu from "../Menu";
+import Carousel from "../Carousel";
+
+export default function Home() {
+  return (
+
+    < div className="home" >
+      {/* <Menu /> */}
+      <Inicio />
+      <Carousel />
+
+    </div >
+
+  );
+}
